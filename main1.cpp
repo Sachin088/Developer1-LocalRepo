08b2008b15725a36c11a0e1ad544d1fe087177b7
Sachin099 <sachinsabale099@gmail.com>
2025-04-19 15:18:19 +0530

    Deleted by Sachin099

---
+++ main1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+std::cout << "Programming is very complicated Gandu language" << std::endl;
+std::cout << "Programming is very complicated Gandu language" << std::endl;
+std::cout << "Programming is very complicated Gandu language" << std::endl;
+std::cout << "Programming is very complicated Gandu language" << std::endl;
+std::cout << "Programming is very complicated Gandu language" << std::endl;
+std::cout << "Programming is very complicated Gandu language" << std::endl;
+return 0;
+}
